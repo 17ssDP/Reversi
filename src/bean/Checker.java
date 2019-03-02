461ea249f05df1f84eb82857f64dda7d1bef1d37
Your Name <17302010026@fudan.edu.cn>
2019-03-02 16:45:56 +0800

    Second Try
    Second Try

--- src/bean/Checker.java
+++ src/bean/Checker.java
@@ -4,23 +4,75 @@ import java.awt.*;
 
 public class Checker {
     private int size;
-
+    private Chess[][] chessboard;
     public Checker() {
 
     }
     public Checker(int size) {
         this.size = size;
+        this.chessboard = new Chess[size][size];
+    }
+    public void addChess(Chess chess) {
+        chessboard[chess.getRow() - 'a'][chess.getCol() - 'a'] = chess;
+    }
+    public int computScore(int row, int col, boolean computer) {
+        int score = 0;
+        for(int i = 1; i >= -1; i--) {
+            for(int j = 1; j >= -1; j--) {
+                if(getChess(row - i, col - j) != null && !chessboard[row - i][col - j].getColor() == computer) {
+                    if(getChess(row - 2 * i, col - 2 * j) != null && chessboard[row - 2 * i][col - 2 * j].getColor() == computer)
+                        score++;
+                }
+            }
+        }
+        return score;
+    }
+    public boolean changeColor(int row, int col, boolean color) {
+        for(int i = 1; i >= -1; i--) {
+            for(int j = 1; j >= -1; j--) {
+                if(getChess(row - i, col - j) != null && !chessboard[row - i][col - j].getColor() == color) {
+                    if(getChess(row - 2 * i, col - 2 * j) != null && chessboard[row - 2 * i][col - 2 * j].getColor() == color) {
+                        chessboard[row - i][col - j].setColor(color);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    public boolean inRange(int row, int col) {
+        return row >= 0 && row < size && col >= 0 && col < size;
+    }
+    public Chess getChess(int row, int col) {
+        if(inRange(row, col))
+            return chessboard[row][col];
+        return null;
     }
     public void print() {
  //       Font font = new Font("Courier New", Font.PLAIN, 14);
+        System.out.print(" ");
         for(int i = 0; i < size; i++) {
             System.out.print(" " + (char)('a' + i));
         }
         for(int i = 0; i < size; i++) {
             System.out.print("\n" + (char)('a' + i));
             for(int j = 0; j < size; j++) {
-                System.out.print(" X");
+                if(chessboard[i][j] == null) {
+                    System.out.print(" .");
+                }else if(chessboard[i][j].getColor()) {
+                    System.out.print(" O");
+                }else {
+                    System.out.print(" X");
+                }
+
             }
         }
     }
-}
+
+    public int getSize() {
+        return size;
+    }
+    public Chess[][] getChessboard() {
+        return chessboard;
+    }
+ }
--- src/bean/Reversi.java
+++ src/bean/Reversi.java
@@ -3,17 +3,68 @@ import java.util.Scanner;
 
 public class Reversi {
     private static int dimen;
-    private static char computer;
+    private static boolean computer;
     public static void main(String[] args) {
         initialize();
         Checker checker = new Checker(dimen);
+        checker.addChess(new Chess(true, (char)('a' + dimen / 2 - 1), (char)('a' + dimen / 2 - 1)));
+        checker.addChess(new Chess(false, (char)('a' + dimen / 2), (char)('a' + dimen / 2 - 1)));
+        checker.addChess(new Chess(false, (char)('a' + dimen / 2 - 1), (char)('a' + dimen / 2)));
+        checker.addChess(new Chess(true, (char)('a' + dimen / 2), (char)('a' + dimen / 2)));
         checker.print();
+        boolean isOver = false;
+        while(!isOver){
+            if(!computer) {
+                compuMov(checker);
+
+            }
+        }
     }
     public static void initialize() {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the board dimension: ");
         dimen = input.nextInt();
         System.out.print("Computer plays (X/O): ");
-        computer = input.next().charAt(0);
+        computer = input.next().charAt(0) == 'O';
+    }
+    public static void compuMov(Checker checker){
+        int maxScore = 0;
+        char row = 'a';
+        char col = 'a';
+        for(int i = 0; i < checker.getSize(); i++) {
+            for(int j = 0; j < checker.getSize(); j++) {
+                if(checker.getChessboard()[i][j] == null) {
+                    int score = checker.computScore(i, j, computer);
+                    if(score > maxScore) {
+                        maxScore = score;
+                        row = (char)('a' + i);
+                        col = (char)('a' + j);
+                    }
+                }
+            }
+        }
+        if(maxScore > 0) {
+            checker.changeColor(row - 'a', col - 'a', computer);
+            Chess chess = new Chess(computer, row, col);
+            checker.addChess(chess);
+            System.out.println("\nComputer places " + (computer? 'O' : 'X') + " at " + row + col);
+            checker.print();
+        }else {
+            System.out.print((computer? 'O' : 'X') + "player has no valid move. Enter move for " + (!computer? 'O' : 'X') + "(RowCol): ");
+        }
+    }
+    public static void playerMov(Checker checker) {
+        System.out.print("Enter move for " + (!computer? 'O' : 'X') + "(RowCol): ");
+        Scanner input = new Scanner(System.in);
+        String move = input.next();
+        if(!checker.changeColor(move.charAt(0), move.charAt(1), !computer)) {
+            System.out.println("Invalid move.");
+            System.out.println("Game Over.");
+            System.out.println((computer? 'O' : 'X') + " player wins.");
+            System.exit(0);
+        }
+    }
+    public static void hasValidMove() {
+
     }
 }
